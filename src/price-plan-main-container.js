2e955a314663e45510c79dae4d4a982f7e5309eb
Aleksander <tatrun89@gmail.com>
2020-04-07 14:16:40 +0300

    restyling price-plan-main-container

--- src/price-plan-main-container.js
+++ src/price-plan-main-container.js
@@ -8,15 +8,39 @@ class PricePlanMain extends LitElement {
                 .border{
                     box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .16), 0 2px 10px 0 rgba(0, 0, 0, .12);
                 }
+                .column{
+                    flex-direction: column;
+                }
+                .row{
+                    flex-direction: row;
+                }
+                .container{
+                    display: flex;
+                }
                 .main-container{
                     display: flex;
                     margin: 15px;                   
                 }
+                .container p, input, button{
+                    margin: 5px;
+                }
                                      
             </style>
             
-            <div class="main-container">
-              <p>Тариф: ${this.pricePlan.name}</p>
+            <div class="main-container column">
+              <p>Редагування тарифного плану</p>
+              <div class="container row">
+                <p>Тариф: </p>
+                <input .value="${this.pricePlan.name}">
+              </div>
+              <div class="container row">
+                <p>Відсоток: </p>
+                <input .value="${this.pricePlan.commissionFree}">
+              </div>
+              <div class="container row">
+                <button>Зберегти</button>
+                <button>Видалити</button>
+              </div>  
             </div>
 
     `;}
